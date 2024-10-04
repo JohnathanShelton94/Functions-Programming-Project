e581b1ea3adf2fcf23b833320ef838342f175f34
Johnathan Shelton <js24634@email.vccs.edu>
2024-10-04 19:17:50 -0400

    Add project files.

---
+++ Functions Programming Project.cpp
@@ -0,0 +1,9 @@
+// Functions Programming Project.cpp : This page is the start of the Functions Programming Projects.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "This is the master page for the Functions Programming Project!\n";
+}
